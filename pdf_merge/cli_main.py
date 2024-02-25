e45e0cdf681a4f0eaed3298325fe15a76eef88d6
dangameiro <66157634+dangameiro@users.noreply.github.com>
2024-02-25 14:01:27 -0500

    Fixed command line script

--- pdf_merge/cli_main.py
+++ pdf_merge/cli_main.py
@@ -1,9 +1,10 @@
 import pdf_merge
 import sys
-  
+
 if (len(sys.argv) < 3):
     print("Usage: python pdf-merge.py front.pdf back.pdf [output.pdf]")
-        
+    exit()
+
 front = sys.argv[1]
 back = sys.argv[2]
 
@@ -12,4 +13,5 @@ output='pdf_out.pdf'
 if (len(sys.argv) == 4):
     output = sys.argv[3]
 
-pdf_merge.pdf_merge(front, back, output)
+def cli_main():
+    pdf_merge.pdf_merge(front, back, output)
